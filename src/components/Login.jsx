c0a8fac8c55dc39803c9e837888cd966be08c467
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-08 00:14:36 +0530

    add: implement controlled login form with reset functionality...

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,67 @@
+import { useState } from "react";
+
 export default function Login() {
-  return (
-    <form>
-      <h2>Login</h2>
+   const [enteredValue, setEnteredValue] = useState({
+      email: "",
+      password: "",
+   });
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
-        </div>
+   const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log("submitted");
+      console.log(enteredValue);
+   };
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
-        </div>
-      </div>
+   const handleInputChange = (type, event) => {
+      setEnteredValue((prev) => ({
+         ...prev,
+         [type]: event,
+      }));
+   };
 
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
-      </p>
-    </form>
-  );
+   const handleReset = () => {
+      setEnteredValue({
+         email: "",
+         password: "",
+      });
+   };
+
+   return (
+      <form onSubmit={handleSubmit}>
+         <h2>Login</h2>
+
+         <div className="control-row">
+            <div className="control no-margin">
+               <label htmlFor="email">Email</label>
+               <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  onChange={(e) => handleInputChange("email", e.target.value)}
+                  value={enteredValue.email}
+               />
+            </div>
+
+            <div className="control no-margin">
+               <label htmlFor="password">Password</label>
+               <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  onChange={(e) =>
+                     handleInputChange("password", e.target.value)
+                  }
+                  value={enteredValue.password}
+               />
+            </div>
+         </div>
+
+         <p className="form-actions">
+            <button className="button button-flat" onClick={handleReset}>
+               Reset
+            </button>
+            <button className="button">Login</button>
+         </p>
+      </form>
+   );
 }
